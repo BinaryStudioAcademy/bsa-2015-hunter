d382f78d8ce5ad88755454227e8e796a8f7d9974
alexkaduk <o.kaduk@gmail.com>
2015-08-02 20:25:15 +0300

    #13: logger update

---
+++ Hunter/Hunter.Common/Concrete/Logger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hunter.Common.Interfaces;
+using log4net;
+
+namespace Hunter.Common.Concrete
+{
+    public class Logger : ILogger
+    {
+        public static readonly ILog _log = LogManager.GetLogger(String.Empty);
+
+        public void Log(Exception ex)
+        {
+            _log.Error(ex.Message, ex);
+        }
+    }
+}
