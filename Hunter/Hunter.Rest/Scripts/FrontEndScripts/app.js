039715110694f71c1600e0d23796268e9086fcc0
iplot <iplotnikov94@gmail.com>
2015-08-03 11:49:42 +0300

    27-add-app-js-basic-routes: Rename module to hunter-app. Add alias for controller in routes

--- Hunter/Hunter.Rest/Scripts/FrontEndScripts/app.js
+++ Hunter/Hunter.Rest/Scripts/FrontEndScripts/app.js
@@ -1,64 +1,76 @@
-angular.module('app', ['ngRoute'])
+angular.module('hunter-app', ['ngRoute'])
 	.config(['$routeProvider', function($routeProvider){
 		$routeProvider
 			//slide 1
 			.when('/candidates', {
 				templateUrl: '',
-				controller: 'candidatesController'
+				controller: 'candidatesController',
+				controllerAs: 'candidatesCtrl'
 			})
 			//result on Latest activity button click (slide 1 and menu item on slide 3)
 			.when('/news', {
 				templateUrl: '',
-				controller: 'newsController'
+				controller: 'newsController',
+				controllerAs: 'newsCtrl'
 			})
 			//result on statistics button click (slide 1)
 			.when('/statistics', {
 				templateUrl: '',
-				controller: 'statisticsController'
+				controller: 'statisticsController',
+				controllerAs: 'statisticsCtrl'
 			})
 			//slide 2
 			.when('/candidates/viewprofile/:profileId', {
 				templateUrl: '',
-				controller: 'profileController'
+				controller: 'profileController',
+				controllerAs: 'profileCtrl'
 			})
 			//slide 3
 			.when('/add', {
 				templateUrl: '',
-				controller: 'createCandidateController'
+				controller: 'createCandidateController',
+				controllerAs: 'createCandidateCtrl'
 			})
 			//slide 4 5
 			.when('/candidates/:profileId', {
 				templateUrl: '',
-				controller: 'profileController'
+				controller: 'profileController',
+				controllerAs: 'profileCtrl'
 			})
 			//slide 6
 			.when('/candidates/:profileId/specnotes', {
 				templateUrl: '',
-				controller: 'specnotesController'
+				controller: 'specnotesController',
+				controllerAs: 'specnotesCtrl',
 			})
 			//slide 6 (history bitton click result)
 			.when('/candidates/:profileId/specnotes/history', {
 				templateUrl: '',
-				controller: 'specnotesController'
+				controller: 'specnotesController',
+				controllerAs: 'specnotesCtrl'
 			})
 			//slide 8
 			.when('/candidates/:profileId/interview', {
 				templateUrl: '',
-				controller: 'interviewController'
+				controller: 'interviewController',
+				controllerAs: 'interviewCtrl'
 			})
 			//slide 8 (history bitton click result)
 			.when('/candidates/:profileId/interview/history', {
 				templateUrl: '',
-				controller: 'interviewController'
+				controller: 'interviewController',
+				controllerAs: 'interviewCtrl'
 			})
 			//slide 7
 			.when('/candidates/:profileId/testwork', {
 				templateUrl: '',
-				controller: 'testworkController'
+				controller: 'testworkController',
+				controllerAs: 'testworkCtrl'
 			})
 			//slide 7 (history bitton click result)
 			.when('/candidates/:profileId/testwork/history', {
 				templateUrl: '',
-				controller: 'testworkController'
+				controller: 'testworkController',
+				controllerAs: 'testworkCtrl'
 			})
 	}]);
