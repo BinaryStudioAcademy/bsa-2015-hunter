a2bfead2193a235f52796b5b6f62d2f55d7712cc
Ihor Serkov <serkov.ihor@gmail.com>
2015-08-05 16:11:13 +0300

    simplify password policy

--- Hunter/Hunter.Rest/App_Start/IdentityConfig.cs
+++ Hunter/Hunter.Rest/App_Start/IdentityConfig.cs
@@ -155,10 +155,10 @@ namespace Hunter.Rest
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
+                RequireNonLetterOrDigit = false,
                 RequireDigit = true,
                 RequireLowercase = true,
-                RequireUppercase = true,
+                RequireUppercase = false,
             };
 
             var dataProtectionProvider = Startup.DataProtectionProvider;
