1dc96815d6220e44d671db672bd9a0960230dfae
Dmitriy Beseda <besedadg@gmail.com>
2015-08-06 12:11:28 +0300

    Added empty Auth Service Factory

---
+++ Hunter/Hunter.Rest/Views/services/helper/authService.js
@@ -0,0 +1,27 @@
+﻿(function() {
+    'use strict';
+
+    angular
+        .module('hunter-app')
+        .factory('AuthService', AuthService);
+
+    AuthService.$inject = [];
+
+    function AuthService() {
+        var service = {
+            isLoggedIn: isLoggedIn
+        };
+
+        function isLoggedIn() {
+            /*
+                if (some) {
+                    return true;
+                } else {
+                    return false;
+                }
+            */
+        }
+
+        return service;
+    }
+})();
