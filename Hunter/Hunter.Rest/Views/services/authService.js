66b365286a67c4e23728f9900cfd08379fa02f9e
Ihor Serkov <serkov.ihor@gmail.com>
2015-08-06 11:08:54 +0300

    #47 - add auth services

---
+++ Hunter/Hunter.Rest/Views/services/authService.js
@@ -0,0 +1,89 @@
+﻿'use strict';
+app.factory('authService', ['$http', '$q', 'localStorageService', function ($http, $q, localStorageService ) {
+
+   
+    var authServiceFactory = {};
+
+    var _authentication = {
+        isAuth: false,
+        userName: ""
+    };
+
+   
+    var _saveRegistration = function (registration) {
+
+        _logOut();
+
+        return $http.post('/api/account/register', registration).then(function (response) {
+            return response;
+        });
+
+    };
+
+    var _login = function (loginData) {
+
+        var data = "grant_type=password&username=" + loginData.userName + "&password=" + loginData.password;
+
+        if (loginData.useRefreshTokens) {
+            data = data ;
+        }
+
+        var deferred = $q.defer();
+
+        $http.post('/token', data, { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }).success(function (response) {
+
+            if (loginData.useRefreshTokens) {
+                localStorageService.set('authorizationData', { token: response.access_token, userName: loginData.userName, refreshToken: response.refresh_token, useRefreshTokens: true });
+            }
+            else {
+                localStorageService.set('authorizationData', { token: response.access_token, userName: loginData.userName, refreshToken: "", useRefreshTokens: false });
+            }
+            _authentication.isAuth = true;
+            _authentication.userName = loginData.userName;
+            _authentication.useRefreshTokens = loginData.useRefreshTokens;
+
+            deferred.resolve(response);
+
+        }).error(function (err, status) {
+            _logOut();
+            deferred.reject(err);
+        });
+
+        return deferred.promise;
+
+    };
+
+    var _logOut = function () {
+
+        localStorageService.remove('authorizationData');
+
+        _authentication.isAuth = false;
+        _authentication.userName = "";
+        _authentication.useRefreshTokens = false;
+
+    };
+
+    var _fillAuthData = function () {
+
+        var authData = localStorageService.get('authorizationData');
+        if (authData) {
+            _authentication.isAuth = true;
+            _authentication.userName = authData.userName;
+            _authentication.useRefreshTokens = authData.useRefreshTokens;
+        }
+
+    };
+
+  
+
+    
+
+
+    authServiceFactory.saveRegistration = _saveRegistration;
+    authServiceFactory.login = _login;
+    authServiceFactory.logOut = _logOut;
+    authServiceFactory.fillAuthData = _fillAuthData;
+    authServiceFactory.authentication = _authentication;
+
+    return authServiceFactory;
+}]);
