aa779540161b1c577b325c73d297088f94aa3862
Dmitriy Beseda <besedadg@gmail.com>
2015-08-07 10:21:51 +0300

    Http Handler body fix

--- Hunter/Hunter.Rest/Views/services/helper/httpHandler.js
+++ Hunter/Hunter.Rest/Views/services/helper/httpHandler.js
@@ -39,7 +39,7 @@
             var config = {
                 'method': httpObject.verb.toUpperCase(),
                 'url': httpObject.url,
-                'body': httpObject.body
+                'data': httpObject.body
             };
 
             $http(config)
