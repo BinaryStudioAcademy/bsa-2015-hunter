41c54bf8581a0e935fd5371c18a7e3bb839db3fb
iplot <iplotnikov94@gmail.com>
2015-08-07 22:44:03 +0300

    60-implement-filter-for-candidates-page: Add filter stub for candidates

---
+++ Hunter/Hunter.Rest/Views/services/candidate/candidatesFilter.js
@@ -0,0 +1,63 @@
+﻿(function() {
+    'use strict';
+
+    angular
+        .module('hunter-app')
+        .filter('CandidatesFilter', CandidatesFilter);
+
+    function filterCandidatesByPool(candidate, poolFilters) {
+        var result = false;
+
+        for (var i in poolFilters) {
+            if (!poolFilters[i].isChecked) {
+                continue;
+            }
+
+            candidate.Pools.forEach(function(candPool) {
+                if (candPool.Name == poolFilters[i].Name) {
+                    result = true;
+                }
+            });
+
+            if (result) {
+                break;
+            }
+        }
+
+        return result;
+    }
+
+    function filterCandidatesByStatus(candidate, statusFilters) {
+        var result = false;
+
+
+    }
+
+    function filterCandidatesByInviter(candidate, inviterFilters) {
+        var result = false;
+
+        for (var i in inviterFilters) {
+            if (inviterFilters[i].isChecked && inviterFilters[i].inviterId == candidate.AddedByProfileId) {
+                result = true;
+                break;
+            }
+        }
+
+        return result;
+    }
+
+    function CandidatesFilter() {
+
+        return function(candidate, options) {
+            if (
+                filterCandidatesByPool(candidate, options.poolFilters) ||
+                filterCandidatesByInviter(candidate, options.inviterFilters)
+            ) {
+                return true;
+            } else {
+                return false;
+            }
+        };
+    }
+
+})();
