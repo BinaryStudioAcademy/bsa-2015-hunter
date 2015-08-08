4249c5d49a41f32a39b185bfe1b6969cf8258963
iplot <iplotnikov94@gmail.com>
2015-08-08 13:11:52 +0300

    60-implement-filter-for-candidates-page: Create candidate filters by pool and recruiter. Add function to fake data in candidateController

--- Hunter/Hunter.Rest/Views/Candidate/list/candidateListtController.js
+++ Hunter/Hunter.Rest/Views/Candidate/list/candidateListtController.js
@@ -18,6 +18,25 @@
         //Here we should write all vm variables default values. For Example:
         vm.name = "Candidates";
 
+        var pools = {
+            '.NET': true,
+            'JS': true,
+            'PHP': false,
+            'QA': false
+        };
+
+        var inviters = [
+            { 'name': 'Ulyana', 'id': 1, 'isChecked': true },
+            { 'name': 'Kate', 'id': 2, 'isChecked': true }
+        ];
+
+        var options = {
+            'poolFilters': pools,
+            'inviterFilters': inviters
+        };
+
+        vm.filterOptions = options;
+
         //(function() {
         //    // This is function for initialization actions, for example checking auth
         //    if (authService.isLoggedIn()) {
@@ -32,6 +51,19 @@
         candidateHttpService.getCandidateList(function (data) {
             vm.candidateList = data.data;
             console.log(data.data);
+
+            fake();
         });
+
+        function fake() {
+            //pool fake
+            vm.candidateList[0].pools.push({ 'name': '.NET' });
+            vm.candidateList[1].pools.push({ 'name': 'JS' });
+            vm.candidateList[1].pools.push({ 'name': '.NET' });
+
+            //inviter fake
+            vm.candidateList[0].addedByProfileId = 1;
+            vm.candidateList[1].addedByProfileId = 2;
+        }
     }
 })();
--- Hunter/Hunter.Rest/Views/services/candidate/candidatesFilter.js
+++ Hunter/Hunter.Rest/Views/services/candidate/candidatesFilter.js
@@ -1,30 +1,49 @@
 ﻿(function() {
     'use strict';
 
+    Array.prototype.unique = function() {
+        var arr = this.concat();
+
+        if (arr.length == 0) {
+            return [];
+        }
+
+        for (var i = 0; i < arr.length - 1; i++) {
+            for (var j = i+1; j < arr.length; j++) {
+                if (arr[i] === arr[j]) {
+                    arr.splice(j--, 1);
+                }
+            }
+        }
+
+        return arr;
+    }
+
     angular
         .module('hunter-app')
         .filter('CandidatesFilter', CandidatesFilter);
 
-    function filterCandidatesByPool(candidate, poolFilters) {
-        var result = false;
+    function filterCandidatesByPool(candidates, poolFilters) {
+        var filtered = [];
 
-        for (var i in poolFilters) {
-            if (!poolFilters[i].isChecked) {
-                continue;
-            }
-
-            candidate.Pools.forEach(function(candPool) {
-                if (candPool.Name == poolFilters[i].Name) {
-                    result = true;
-                }
-            });
-
-            if (result) {
-                break;
-            }
+        if (candidates == undefined) {
+            return filtered;
         }
 
-        return result;
+        candidates.forEach(function(candidate) {
+            var wasPushed = false;
+
+            candidate.pools.forEach(function(pool) {
+                var name = pool.name;
+
+                if (poolFilters[name] && !wasPushed) {
+                    filtered.push(candidate);
+                    wasPushed = true;
+                }
+            });
+        });
+
+        return filtered;
     }
 
     function filterCandidatesByStatus(candidate, statusFilters) {
@@ -33,31 +52,32 @@
 
     }
 
-    function filterCandidatesByInviter(candidate, inviterFilters) {
-        var result = false;
+    function filterCandidatesByInviter(candidates, inviterFilters) {
+        var filtered = [];
 
-        for (var i in inviterFilters) {
-            if (inviterFilters[i].isChecked && inviterFilters[i].inviterId == candidate.AddedByProfileId) {
-                result = true;
-                break;
-            }
+        if (candidates == undefined) {
+            return filtered;
         }
 
-        return result;
+        candidates.forEach(function(candidate) {
+            inviterFilters.forEach(function(inviter) {
+                if (inviter.id == candidate.addedByProfileId && inviter.isChecked) {
+                    filtered.push(candidate);
+                }
+            });
+        });
+
+        return filtered;
     }
 
     function CandidatesFilter() {
 
-        return function(candidate, options) {
-            if (
-                filterCandidatesByPool(candidate, options.poolFilters) ||
-                filterCandidatesByInviter(candidate, options.inviterFilters)
-            ) {
-                return true;
-            } else {
-                return false;
-            }
-        };
+        return function (candidates, options) {
+            var res = filterCandidatesByPool(candidates, options.poolFilters);
+            res = res.concat(filterCandidatesByInviter(candidates, options.inviterFilters));
+
+            return res.unique();
+        }
     }
 
 })();
