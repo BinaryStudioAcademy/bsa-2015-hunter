78a3aacc0f6cdd8272297e51e22f75e667dfc953
iplot <iplotnikov94@gmail.com>
2015-08-09 14:59:25 +0300

    60-implement-filter-for-candidates-page: Add more fake data into candidateListController

--- Hunter/Hunter.Rest/Views/Candidate/list/candidateListtController.js
+++ Hunter/Hunter.Rest/Views/Candidate/list/candidateListtController.js
@@ -73,17 +73,71 @@
         function fake() {
             //pool fake
             vm.candidateList[0].pools.push({ 'name': '.NET' });
-            vm.candidateList[1].pools.push({ 'name': 'JS' });
             vm.candidateList[1].pools.push({ 'name': '.NET' });
+            vm.candidateList[2].pools.push({ 'name': '.NET' });
+            vm.candidateList[3].pools.push({ 'name': 'JS' });
+            vm.candidateList[4].pools.push({ 'name': 'JS' });
+            vm.candidateList[5].pools.push({ 'name': 'JS' });
+            vm.candidateList[6].pools.push({ 'name': 'PHP' });
+            vm.candidateList[7].pools.push({ 'name': 'PHP' });
+            vm.candidateList[8].pools.push({ 'name': 'PHP' });
+            vm.candidateList[9].pools.push({ 'name': 'QA' });
+            vm.candidateList[10].pools.push({ 'name': 'QA' });
+            vm.candidateList[11].pools.push({ 'name': 'QA' });
+            vm.candidateList[12].pools.push({ 'name': '.NET' });
+            vm.candidateList[13].pools.push({ 'name': '.NET' });
+            vm.candidateList[14].pools.push({ 'name': '.NET' });
+
+            //results:
+            //.NET -> Hollis Sefton; Porter Wystan; Gabe Raven; Gloria Delma; Deanne Imogene; Lon Abner;
+            //JS -> Jack Sylvanus; Lindsay Darryl; Jennie Charlie
+            //PHP -> Gracelyn Moriah; Elizabeth Rona; Sunny Fawn
+            //QA -> Christianne Diantha; Allie Marideth; Kennedy Wardell
+
 
             //inviter fake
             vm.candidateList[0].addedByProfileId = 1;
             vm.candidateList[1].addedByProfileId = 2;
+            vm.candidateList[2].addedByProfileId = 3;
+            vm.candidateList[3].addedByProfileId = 1;
+            vm.candidateList[4].addedByProfileId = 1;
+            vm.candidateList[5].addedByProfileId = 1;
+            vm.candidateList[6].addedByProfileId = 2;
+            vm.candidateList[7].addedByProfileId = 2;
+            vm.candidateList[8].addedByProfileId = 2;
+            vm.candidateList[9].addedByProfileId = 3;
+            vm.candidateList[10].addedByProfileId = 3;
+            vm.candidateList[11].addedByProfileId = 3;
+            vm.candidateList[12].addedByProfileId = 3;
+            vm.candidateList[13].addedByProfileId = 3;
+            vm.candidateList[14].addedByProfileId = 3;
+
+            //results:
+            //Ulyana -> Hollis Sefton; Jack Sylvanus; Lindsay Darryl; Jennie Charlie
+            //Kate -> Porter Wystan; Gracelyn Moriah; Elizabeth Rona; Sunny Fawn
+            //Ira -> Gabe Raven; Christianne Diantha; Allie Marideth; Kennedy Wardell; Gloria Delma; Deanne Imogene; Lon Abner;
 
             //statuses fake
             vm.candidateList[0].resolutionString = 'New';
-            vm.candidateList[1].resolutionString = 'InReview';
-            vm.candidateList[2].resolutionString = 'Hired';
+            vm.candidateList[1].resolutionString = 'New';
+            vm.candidateList[2].resolutionString = 'New';
+            vm.candidateList[3].resolutionString = 'New';
+            vm.candidateList[4].resolutionString = 'New';
+            vm.candidateList[5].resolutionString = 'Hired';
+            vm.candidateList[6].resolutionString = 'Hired';
+            vm.candidateList[7].resolutionString = 'Hired';
+            vm.candidateList[8].resolutionString = 'Hired';
+            vm.candidateList[9].resolutionString = 'Hired';
+            vm.candidateList[10].resolutionString = 'InReview';
+            vm.candidateList[11].resolutionString = 'InReview';
+            vm.candidateList[12].resolutionString = 'InReview';
+            vm.candidateList[13].resolutionString = 'InReview';
+            vm.candidateList[14].resolutionString = 'InReview';
+
+            //result:
+            //New -> Hollis Sefton; Porter Wystan; Gabe Raven; Jack Sylvanus; Lindsay Darryl;
+            //Hired -> Jennie Charlie; Gracelyn Moriah; Elizabeth Rona; Sunny Fawn; Christianne Diantha;
+            //InReview -> Allie Marideth; Kennedy Wardell; Gloria Delma; Deanne Imogene; Lon Abner;
         }
     }
 })();
