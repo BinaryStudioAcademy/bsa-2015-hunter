0f3ae82ea34590cba6cd6df2623376b2a0424a8b
muroslavko <myroslavdm@gmail.com>
2015-08-09 23:15:14 +0300

    61-implement-filter-for-vacancies-page: add pool filter

--- Hunter/Hunter.Rest/Views/Vacancy/list/vacancyListController.js
+++ Hunter/Hunter.Rest/Views/Vacancy/list/vacancyListController.js
@@ -12,19 +12,55 @@
 
     VacancyListController.$inject = [
         '$scope',
+        '$filter',
         'VacancyHttpService'
     ];
 
-    function VacancyListController($scope, VacancyHttpService) {
+    function VacancyListController($scope, $filter, VacancyHttpService) {
         var vm = this;
+
+        var pools = {
+            1: false,
+            2: false,
+            3: false,
+            4: false
+        };
+
+        var statuses = {
+            'New': false,
+            'InReview': false,
+            'Hired': false
+        };
+
+        var inviters = [
+            { 'name': 'Ulyana', 'id': 1, 'isChecked': false },
+            { 'name': 'Kate', 'id': 2, 'isChecked': false },
+            { 'name': 'Ira', 'id': 3, 'isChecked': false }
+        ];
+
+        var options = {
+            'poolFilters': pools,
+            'inviterFilters': inviters,
+            'statusFilters': statuses
+        };
+
+        vm.filterOptions = options;
+
 
         vm.currentPage = 1;
         vm.pageSize = 5;
+        vm.vacancies;
 
         VacancyHttpService.getVacancies().then(function (result) {
             console.log(result);
             vm.vacancies = result;
         });
+
+        function toggleCheckbox() {
+            $filter('VacanciesFilter')(vm.vacancies, vm.filterOptions);
+        }
+
+        vm.toggle = toggleCheckbox;
     }
 
     function OtherController() {
---
+++ Hunter/Hunter.Rest/Views/filters/vacancies/vacanciesListFilter.js
@@ -0,0 +1,118 @@
+﻿(function () {
+    'use strict';
+
+    Array.prototype.unique = function () {
+        var arr = this.concat();
+
+        if (arr.length == 0) {
+            return [];
+        }
+
+        for (var i = 0; i < arr.length - 1; i++) {
+            for (var j = i + 1; j < arr.length; j++) {
+                if (arr[i] === arr[j]) {
+                    arr.splice(j--, 1);
+                }
+            }
+        }
+
+        return arr;
+    }
+
+    angular
+        .module('hunter-app')
+        .filter('VacanciesFilter', VacanciesFilter);
+
+    function filterVacanciesByPool(vacancies, poolFilters) {
+        var filtered = [];
+
+        if (vacancies == undefined) {
+            return filtered;
+        }
+
+        vacancies.forEach(function (vacancy) {
+            var id = vacancy.poolId;
+
+                if (poolFilters[id]) {
+                    filtered.push(vacancy);
+                }
+        });
+
+        return filtered;
+    }
+
+    function filterVacanciesByStatus(vacancies, statusFilters) {
+        var filtered = [];
+
+        if (vacancies == undefined) {
+            return filtered;
+        }
+
+        vacancies.forEach(function (vacancy) {
+            var resolution = vacancy.resolutionString;
+
+            if (statusFilters[resolution]) {
+                filtered.push(vacancy);
+            }
+        });
+
+        return filtered;
+    }
+
+    function filterVacanciesByAdder(vacancies, inviterFilters) {
+        var filtered = [];
+
+        if (vacancies == undefined) {
+            return filtered;
+        }
+
+        vacancies.forEach(function (vacancy) {
+            inviterFilters.forEach(function (inviter) {
+                if (inviter.id == vacancy.addedByProfileId && inviter.isChecked) {
+                    filtered.push(vacancy);
+                }
+            });
+        });
+
+        return filtered;
+    }
+
+    function VacanciesFilter() {
+
+        return function (vacancies, options) {
+
+            if (!checkOptions(options.poolFilters) &&
+                !checkOptions(options.statusFilters) &&
+                !checkOptions(options.inviterFilters)) {
+                return vacancies;
+            }
+
+            var res = filterVacanciesByPool(vacancies, options.poolFilters);
+            //res = res.concat(filterCandidatesByInviter(candidates, options.inviterFilters));
+            //res = res.concat(filterCandidatesByStatus(candidates, options.statusFilters));
+
+            return res;
+        }
+    }
+
+    function checkOptions(options) {
+        var anyChecked = false;
+
+        if (options instanceof Array) {
+            for (var i in options) {
+                if (options[i].isChecked) {
+                    anyChecked = true;
+                }
+            }
+        } else {
+            for (var key in options) {
+                if (options[key]) {
+                    anyChecked = true;
+                }
+            }
+        }
+
+        return anyChecked;
+    }
+
+})();
