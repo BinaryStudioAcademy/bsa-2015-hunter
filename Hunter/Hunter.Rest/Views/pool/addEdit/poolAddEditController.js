0c63393dc4191b110f81fb3a295fcae174ea1c8e
Beseda Dmitriy <BesedaDG@gmail.com>
2015-08-09 08:44:21 +0300

    change timeout -> callback logic

--- Hunter/Hunter.Rest/Views/pool/addEdit/poolAddEditController.js
+++ Hunter/Hunter.Rest/Views/pool/addEdit/poolAddEditController.js
@@ -9,11 +9,10 @@
         "$location",
         "AuthService",
         "HttpHandler",
-        "$routeParams",
-        "$timeout"
+        "$routeParams"
     ];
 
-    function PoolAddEditController($location, AuthService, HttpHandler, $routeParams, $timeout) {
+    function PoolAddEditController($location, AuthService, HttpHandler, $routeParams) {
         var vm = this;
         vm.pageConfig = {};
         vm.id = 0;
@@ -44,11 +43,10 @@
                     body: JSON.stringify(vm.pool),
                     successCallback: function (result) {
                         //console.log(result);
+                        $location.url("/pool")
                     },
                     errorCallback: function (result) { console.log(result); }
                 });
-
-                $timeout(function() { $location.url("/pool") }, 300);
             };
 
             vm.poolDelete = function () {
@@ -57,11 +55,10 @@
                     verb: "DELETE",
                     successCallback: function (result) {
                         //console.log(result);
+                        $location.url("/pool")
                     },
                     errorCallback: function (result) { console.log(result); }
                 });
-
-                $timeout(function () { $location.url("/pool") }, 300);
             };
         }
         // add - post
@@ -89,11 +86,10 @@
                     body: JSON.stringify(vm.pool),
                     successCallback: function (result) {
                         console.log(result);
+                        $location.url('/pool');
                     },
                     errorCallback: function (result) { console.log(result); }
                 });
-
-                $timeout(function () { $location.url("/pool") }, 300);
             }
         }
 
