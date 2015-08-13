4ce48c9266487411501b9e260b7379b4e05284fa
devSlike <aloha-bit@mail.ru>
2015-08-13 18:50:43 +0300

    add sorting

--- Hunter/Hunter.Rest/Views/Vacancy/list/vacancyListController.js
+++ Hunter/Hunter.Rest/Views/Vacancy/list/vacancyListController.js
@@ -36,12 +36,25 @@
             vm.pools = data;
         });
 
-        vm.statuses = [{ id: 0, name: 'Active' }, { id: 1, name: 'Closed' }, { id: 2, name: 'Burning' }];
+        vm.statuses = [
+            { id: 0, name: 'Active' },
+            { id: 1, name: 'Closed' },
+            { id: 2, name: 'Burning' }
+        ];
         vm.adders = ["recruiter@local.com", "recruiter2@local.com", "recruiter3@local.com"];
+        vm.sortBy = [
+            { name: "Add Date (new first)", reverseSort: true, sortColumn: "startDate" },
+            { name: "Add Date (old first)", reverseSort: false, sortColumn: "startDate" },
+            { name: "Name (A-Z)", reverseSort: false, sortColumn: "name" },
+            { name: "Name (Z-A)", reverseSort: true, sortColumn: "name" },
+        ];
+        vm.sortAction = vm.sortBy[0];
 
         vm.vacancies = [];
 
         vm.loadDataByParams = function () {
+            vm.filterParams.sortColumn = vm.sortAction.sortColumn;
+            vm.filterParams.reverceSort = vm.sortAction.reverseSort;
             vacancyHttpService.getFilteredVacancies(vm.filterParams).then(function (result) {
                 vm.vacancies = result;
             });
