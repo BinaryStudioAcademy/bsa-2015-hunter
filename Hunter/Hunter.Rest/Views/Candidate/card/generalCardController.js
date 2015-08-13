1382afcdd5bc363034997c6df9a745dfdd36f65b
kuwkuw <kuw-kuw@mail.ru>
2015-08-13 20:19:52 +0300

    #98 Card Static Part added view

--- Hunter/Hunter.Rest/Views/Candidate/card/generalCardController.js
+++ Hunter/Hunter.Rest/Views/Candidate/card/generalCardController.js
@@ -8,17 +8,25 @@
     GeneralCardController.$inject = [
         '$routeParams',
         '$scope',
+        'CandidateHttpService',
         'CardService'
     ];
 
-    function GeneralCardController($routeParams, $scope, cardService) {
+    function GeneralCardController($routeParams, $scope, candidateHttpService, cardService) {
         var vm = this;
         vm.templateToShow = '';
         vm.tabs = ['Overview', 'Special Notes', 'HR Interview', 'Technical Interview', 'Test'];
+        vm.candidate;
 
         vm.changeTemplate = changeTemplate;
 
-        console.log($routeParams);
+        console.log("rout",$routeParams);
+
+        candidateHttpService.getCandidate($routeParams["cid"]).then(function (response) {
+            vm.candidate = response.data;
+            console.log(response.data);
+        });
+
 
         function changeTemplate(templateName) {
             vm.templateToShow = cardService.changeTemplate(templateName);
