69e9feb5e0a126ea046290f9116a878832700314
devSlike <aloha-bit@mail.ru>
2015-08-14 13:07:21 +0300

    #91 - Bring to the global status enumerations constant values (Add constants and synchronize with server enums)

---
+++ Hunter/Hunter.Rest/Views/services/constants/enumConstants.js
@@ -0,0 +1,39 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('hunter-app')
+        .constant('EnumConstants', {
+            origins: [
+                { id: 0, name: 'Sourced'},
+                { id: 1, name: 'Applied' }
+            ],
+            resolutions: [
+                { id: 0, name: 'None' },
+                { id: 1, name: 'Hired' },
+                { id: 2, name: 'Available' },
+                { id: 3, name: 'Not Now' },
+                { id: 4, name: 'Not Interested' },
+                { id: 5, name: 'Unfit' },
+            ],
+            vacancyStates: [
+                { id: 0, name: 'Open' },
+                { id: 1, name: 'Closed' },
+                { id: 1, name: 'Burning123' }
+            ],
+            substatuses: [
+                { id: 0, name: 'Test Failed' },
+                { id: 1, name: 'Interview Failed' },
+                { id: 2, name: 'Passed' },
+            ],
+            cardStage: [
+                { id: 0, name: 'Test Send' },
+                { id: 1, name: 'Test Done' },
+                { id: 2, name: 'Interview' },
+                { id: 3, name: 'Failed' },
+                { id: 4, name: 'Test Failed' },
+                { id: 5, name: 'Interview Failed' },
+                { id: 6, name: 'Passed' },
+            ]
+        });
+})();
