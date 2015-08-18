010d07f3e5d333b95b23346752b1743f15f2c6f0
kuwkuw <kuw-kuw@mail.ru>
2015-08-18 14:00:23 +0300

    #121 Highlight selected menu item

--- Hunter/Hunter.Rest/Views/Home/indexController.js
+++ Hunter/Hunter.Rest/Views/Home/indexController.js
@@ -6,14 +6,16 @@
         .controller('IndexController', IndexController);
 
     IndexController.$inject = [
+        '$scope',
         'IndexHttpService',
         '$interval'
     ];
 
-    function IndexController(IndexHttpService, $interval) {
+    function IndexController($scope, IndexHttpService, $interval) {
         var vm = this;
-
         vm.name = "Index";
+        $scope.radioModel = 'Home';
+
         getActivityAmount();
 
         $interval(getActivityAmount, 180000);   //3 minutes
