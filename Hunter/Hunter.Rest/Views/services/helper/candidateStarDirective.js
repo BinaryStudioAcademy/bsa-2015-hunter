2df9081e8d93e2239d42ab13e44af18d864dc96e
Ihor Serkov <serkov.ihor@gmail.com>
2015-08-19 22:45:51 +0300

    #147 - star template clickable

---
+++ Hunter/Hunter.Rest/Views/services/helper/candidateStarDirective.js
@@ -0,0 +1,29 @@
+﻿(function () {
+    'use strict';
+    angular
+        .module('hunter-app')
+        .directive('shorListStar',shorListStar);
+
+    shorListStar.$inject = [];
+
+    function shorListStar() {
+            return {
+                template: '<i class="fa " ' +
+                    'ng-class="fill ? \'fa-star-o\' : \'fa-star\' "' +
+                    'ng-click="fill = !fill" ' +
+                    'style="position: absolute;font-size: large;top: 2px;left: 2px;color: gold;"' +
+                    '></i>',
+                restrict: 'A',
+                transclude: true,
+                link: function (scope, element, attrs, ctrl, transclude) {
+                    scope.fill = false;
+
+                    transclude(function (clone) {
+                        element.append(clone);
+                    });
+
+                }
+            }
+        }
+
+})();
