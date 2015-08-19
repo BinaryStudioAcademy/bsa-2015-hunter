fe3b6efe23dc3c5c43763a94e1c14a99eebbf1fc
iplot <iplotnikov94@gmail.com>
2015-08-19 20:48:31 +0300

    97-card-test-tab: Fix button changing on Technical Interview tab

--- Hunter/Hunter.Rest/Views/Candidate/card/cardTechnicalInterviewController.js
+++ Hunter/Hunter.Rest/Views/Candidate/card/cardTechnicalInterviewController.js
@@ -8,14 +8,16 @@
     CardTechnicalInterviewController.$inject = [
         'FeedbackHttpService',
         '$routeParams',
-        'VacancyHttpService'
+        'VacancyHttpService',
+        'localStorageService'
     ];
 
-    function CardTechnicalInterviewController(FeedbackHttpService, $routeParams, VacancyHttpService) {
+    function CardTechnicalInterviewController(FeedbackHttpService, $routeParams, VacancyHttpService, localStorageService) {
         var vm = this;
         vm.templateName = 'Technical Interview';
         vm.techFeedback;
         vm.vacancy;
+        var userName = localStorageService.get('authorizationData').userName;
 
         VacancyHttpService.getVacancy($routeParams.vid).then(function (result) {
             vm.vacancy = result;
@@ -30,6 +32,19 @@
             }
             FeedbackHttpService.saveFeedback(body, $routeParams.vid, $routeParams.cid).then(function (result) {
                 vm.techFeedback = result;
+                //!!!!!!
+                if (vm.techFeedback.text == '') {
+                    vm.techFeedback.feedbackConfig = {
+                        'buttonName': 'Save',
+                        'readOnly': false
+                    }
+                } else {
+                    vm.techFeedback.feedbackConfig = {
+                        'buttonName': 'Edit',
+                        'readOnly': true
+                    }
+                }
+                //!!!!!!!
                 console.log("result after update");
                 console.log(result);
             });
@@ -38,10 +53,34 @@
 
         FeedbackHttpService.getTechFeedback($routeParams.vid, $routeParams.cid).then(function (result) {
             vm.techFeedback = result;
+
+            if (vm.techFeedback.text == '') {
+                vm.techFeedback.feedbackConfig = {
+                    'buttonName': 'Save',
+                    'readOnly': false
+                }
+            } else {
+                vm.techFeedback.feedbackConfig = {
+                    'buttonName': 'Edit',
+                    'readOnly': true
+                }
+            }
+
             console.log(result);
         });
 
+        vm.toggleReadOnly = function () {
+            vm.techFeedback.feedbackConfig.readOnly = !vm.techFeedback.feedbackConfig.readOnly;
 
+            if (vm.techFeedback.feedbackConfig.readOnly) {
+                vm.techFeedback.feedbackConfig.buttonName = 'Edit';
+                vm.saveFeedback(vm.techFeedback.id, vm.techFeedback.cardId, vm.techFeedback.text, vm.techFeedback.type);
+//                feedback.userName = userName;
+//                feedback.date = new Date();
+            } else {
+                vm.techFeedback.feedbackConfig.buttonName = 'Save';
+            }
+        }
 
     }
 })();
