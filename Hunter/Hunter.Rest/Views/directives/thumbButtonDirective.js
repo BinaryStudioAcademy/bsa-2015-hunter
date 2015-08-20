25e5bd2d41a7b555c329017d0c25484c68e1ee27
iplot <iplotnikov94@gmail.com>
2015-08-21 00:09:14 +0300

    148-adjust-like-dislike-buttons-for-feedbacks: Create directive for 'vote' buttons

---
+++ Hunter/Hunter.Rest/Views/directives/thumbButtonDirective.js
@@ -0,0 +1,41 @@
+﻿(function() {
+    'use strict';
+
+    angular
+        .module('hunter-app')
+        .directive('thumbButton', ThumbButton);
+
+    ThumbButton.$inject = [];
+
+    function ThumbButton() {
+        return {
+            template:
+                    '<div id="likeBtn" ng-click="click()" class="circleButton">' +
+                        '<span id="likeIcon" class="fa circleButton-icon"></span>' +
+                    '</div>',
+            scope: {
+                btnType: '@btnType',
+                btnIcon: '@btnIcon',
+                feedConfig: '=feedConfig',
+                borderColor: '@borderColor'
+            },
+            restrict: 'EA',
+            link: function (scope, elem, attr) {
+                var button = $(elem).find('.circleButton')[0];
+                var icon = $(button).find('.circleButton-icon')[0];
+
+                $(icon).addClass(scope.btnIcon);
+                $(button).addClass(scope.btnType);
+
+                scope.click = function() {
+
+                    if (scope.feedConfig.style == undefined || scope.feedConfig.style["border-color"] != scope.borderColor) {
+                        scope.feedConfig.style = { 'border-color': scope.borderColor }
+                    } else {
+                        scope.feedConfig.style = {'border-color': 'gray'}
+                    }
+                }
+            }
+        }
+    }
+})()
