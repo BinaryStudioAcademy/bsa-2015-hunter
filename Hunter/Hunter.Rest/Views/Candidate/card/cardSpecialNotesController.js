920432384d4f1ebc906deed4b155b0347f3ae97a
Anton Vasilenko <toxa07@gmail.com>
2015-08-22 19:17:32 +0300

    114-special-notes-refactor-labels-about-editing-note special notes UI & controller simpilfy

--- Hunter/Hunter.Rest/Views/Candidate/card/cardSpecialNotesController.js
+++ Hunter/Hunter.Rest/Views/Candidate/card/cardSpecialNotesController.js
@@ -17,13 +17,13 @@
         vm.templateName = 'Special Notes';
 
         vm.cardNoteShow = getAllCardNote;
-        vm.candidateNoteShow = getCandidateNote;
-        vm.userNoteShow = getUserNote;
-
-        vm.toggleReadOnly = toggleReadOnly;
+        //vm.candidateNoteShow = getCandidateNote;
+        //vm.userNoteShow = getUserNote;
+        vm.saveOldNote = saveOldNote;
+        vm.saveNewNote = saveNewSpecialNote;
 
         vm.vacancy;
-        vm.specialNote;
+        vm.notes;
         vm.userName = localStorageService.get('authorizationData').userName;
         console.log(vm.userName);
         vm.newNoteText;
@@ -36,32 +36,19 @@
 
         getAllCardNote();
 
-
-        vm.saveNewNote = SaveNewSpecialNote;
-        vm.saveOldNote = SaveOldSpecialNote;
-
-        function SaveNewSpecialNote(newText) {
+        function saveNewSpecialNote() {
             var note = {
-                text : newText
-            }
+                text: vm.newNoteText
+            };
             specialNoteHttpService.addSpecialNote(note, $routeParams.vid, $routeParams.cid)
-            .then(function(data) {
-                note.id = data.id;
-                note.lastEdited = data.update;
-                note.userLogin = data.userName;
-                note.cardId = data.cardId;
-                note.noteConfig = {
-                    'buttonName': 'Edit',
-                    'readOnly': true
-                };
-
-                vm.specialNote.push(note);
+            .then(function (data) {
+                vm.notes.push(data);
             });
         }
 
-        function SaveOldSpecialNote(note) {
+        function saveOldSpecialNote(note) {
             specialNoteHttpService.updateSpecialNote(note, note.id, successEdit)
-            .then(function(data) {
+            .then(function (data) {
                 note.id = data.id;
                 note.lastEdited = data.update;
                 note.userLogin = data.userName;
@@ -74,55 +61,35 @@
             vm.newNoteText = "";
             getAllCardNote();
         }
+
         function successEdit(response) {
-            getAllCardNote();
+            //getAllCardNote();
         }
 
         // TODO: Data Functions (not user event functions) Should Be In Services
         function getAllCardNote() {
             specialNoteHttpService.getCardSpecialNote($routeParams.vid, $routeParams.cid).then(function (result) {
                 console.log(result.data);
-                vm.specialNote = result.data;
-
-                vm.specialNote.forEach(function(note) {
-                    note.noteConfig = {
-                        'buttonName': 'Edit',
-                        'readOnly': true
-                    }
-                });
+                vm.notes = result.data;
             });
         }
 
-        function getCandidateNote() {
+        /*function getCandidateNote() {
             specialNoteHttpService.getCandidateSpecialNote($routeParams.cid).then(function (result) {
                 console.log(result.data);
-                vm.specialNote = result.data;
+                vm.notes = result.data;
             });
         }
 
         function getUserNote() {
             specialNoteHttpService.getUserSpecialNote(vm.userName, $routeParams.vid, $routeParams.cid).then(function (result) {
                 console.log(result.data);
-                vm.specialNote = result.data;
+                vm.notes = result.data;
             });
-        }
+        }*/
 
-        function toggleReadOnly(note) {
-            note.noteConfig.readOnly = note.text != '' ? !note.noteConfig.readOnly : note.noteConfig.readOnly;
-
-            if (note.noteConfig.readOnly) {
-                note.noteConfig.buttonName = 'Edit';
-                SaveOldSpecialNote({
-                    'id': note.id,
-                    'userLogin': note.login,
-                    'text': note.text,
-                    'lastEdited': note.lastEdited,
-                    'cardId': note.cardId
-                });
-
-            } else {
-                note.noteConfig.buttonName = 'Save';
-            }
-        }
+        function saveOldNote(note) {
+            saveOldSpecialNote(note);
+        };
     }
 })();
