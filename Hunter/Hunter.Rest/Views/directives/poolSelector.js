f43cb43a64fc2962c1787153f02592e342351d90
iplot <iplotnikov94@gmail.com>
2015-08-27 15:39:25 +0300

    180-add-pool-selectors-in-certain-places: Add simple directive. Startpoint of creation directive and refactoring pools

---
+++ Hunter/Hunter.Rest/Views/directives/poolSelector.js
@@ -0,0 +1,25 @@
+﻿(function() {
+    angular
+        .module('hunter-app')
+        .directive('poolSelector', PoolSelector);
+
+    PoolSelector.$inject = [];
+
+    function PoolSelector() {
+        return {
+            restrict: 'EA',
+            controller: 'PoolListController',
+            controllerAs: 'poolCtrl',
+            link: function(scope, elem, attr) {
+                
+            },
+            template: 
+                '<div style="width: auto; display: inline-block;">' +
+                    '<button class=" btn btn-default"><i class="fa fa-plus"></i></button>' +
+                    '<div style="position: absolute; width: auto; background-color: white; z-index: 100" ng-controller="PoolListController as poolCtrl">' +
+                        '<div ng-include="\'Views/pool/list/list.html\'"></div>' +
+                    '</div>' +
+                '</div>'
+        }
+    }
+})()
