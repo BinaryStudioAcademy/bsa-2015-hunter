c1328ca1f5b15ec0f3fd686373b1eaca2ea2a0ba
devSlike <aloha-bit@mail.ru>
2015-08-27 23:37:39 +0300

    #199 - Remind mechanism (modify server code and start developing client logic)

---
+++ Hunter/Hunter.Rest/Views/notification/list/notificationListController.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    "use strict";
+
+    angular
+        .module("hunter-app")
+        .controller("NotificationListController", NotificationListController);
+
+    NotificationListController.$inject = [
+        "$location",
+        "AuthService",
+        "NotificationHttpService"
+    ];
+
+    function NotificationListController($location, authService, notificationHttpService) {
+        var vm = this;
+        vm.controllerName = 'Notification list';
+    }
+})();
