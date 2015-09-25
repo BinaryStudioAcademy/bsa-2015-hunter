05a8a80a3f54de2b20c3eeed66a97a254e9aa12c
Anton Vasilenko <toxa07@gmail.com>
2015-09-25 11:45:25 +0300

    html5 base rot is added to app.js

--- Hunter/Hunter.Rest/Views/app/app.js
+++ Hunter/Hunter.Rest/Views/app/app.js
@@ -7,8 +7,10 @@ angular.module('hunter-app',
      'ngAnimate', 'monospaced.elastic',
      'ngCookies'])
     .config([
-        '$routeProvider', function ($routeProvider) {
+        '$routeProvider','$locationProvider', function ($routeProvider, $locationProvider) {
             
+            $locationProvider.html5Mode(true);
+
             var viewPath =  "./Views/";
             $routeProvider
                 //result on Latest activity button click (slide 1 and menu item on slide 3)
