b69b06925e535fb7401caa1fc3c28a27d6249c87
Anton Vasilenko <toxa07@gmail.com>
2015-11-11 11:06:30 +0200

    missing file

---
+++ Hunter/Hunter.Rest/Views/app/globalService.js
@@ -0,0 +1,33 @@
+﻿(function() {
+    "use strict";
+
+    angular
+        .module('hunter-app')
+        .factory('globalService', globalService);
+
+    globalService.$inject = [];
+
+    function globalService() {
+
+        var service = {
+            baseUrl: window.globalVars.baseUrl,
+            viewsUrl: window.globalVars.baseUrl + 'Views/'
+        }
+
+        function getUrl(url) {
+            if (window.globalVars.baseUrl === '/') {
+                return url;
+            }
+            return window.globalVars.baseUrl + url;
+        };
+
+        function redirect(url) {
+            var target = getUrl(url);
+            window.location = target;
+        }
+
+        service.url = getUrl;
+        service.redirect = redirect;
+        return service;
+    }
+})();
