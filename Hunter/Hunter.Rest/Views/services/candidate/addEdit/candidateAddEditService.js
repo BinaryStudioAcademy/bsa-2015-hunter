3e646aa11eb77659667e5bdd2e17ecd87b9ec33b
myroslav <myroslavdm@gmail.com>
2015-12-22 12:22:06 +0200

    273-create-candidate-page-email-and-phone-fields-do-not-have-any-validation

--- Hunter/Hunter.Rest/Views/services/candidate/addEdit/candidateAddEditService.js
+++ Hunter/Hunter.Rest/Views/services/candidate/addEdit/candidateAddEditService.js
@@ -38,12 +38,24 @@
                     noErrors = false;
             }
 
-            if (!isNumeric(data.YearsOfExperience) && data.YearsOfExperience != undefined) {            
+            if (!isNumeric(data.YearsOfExperience) && data.YearsOfExperience != undefined && data.YearsOfExperience != "") {            
                     errorObject.message += ' Years Of Experience; ';
                     noErrors = false;
             }
-            if (!isNumeric(data.Salary) && data.Salary != undefined) {            
+            if (!isNumeric(data.Salary) && data.Salary != undefined && data.Salary != "") {            
                     errorObject.message += ' Salary; ';
+                    noErrors = false;
+            }
+
+            if (!isNumeric(data.Phone) && data.Phone != undefined && data.Phone != "") {            
+                    errorObject.message += ' Phone; ';
+                    noErrors = false;
+            }
+
+            var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+            if (!re.test(data.Email) && data.Email != undefined && data.Email != "") {            
+                    errorObject.message += ' Email; ';
                     noErrors = false;
             }
 
